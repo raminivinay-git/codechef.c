7cf21c79611c65982df05c30891dc7180cc513e8
raminivinay777-hub <raminivinay777@gmail.com>
2026-05-13 19:07:45 +0530

    Vacation Clothes.c

---
+++ Vacation Clothes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, d;
+    scanf("%d", & n);
+    if (n < 7) {
+        printf("%d", n);
+    }
+    else {
+        printf("7");
+    }
+    return 0;
+
+}
