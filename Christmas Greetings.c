ecd8fd7767e2dff91d0f7cc16c10bdda662f8cf0
raminivinay777-hub <raminivinay777@gmail.com>
2026-05-20 16:14:50 +0530

    Christmas Greetings.c

---
+++ Christmas Greetings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d", & x);
+    if (x == 25) {
+        printf("CHRISTMAS");
+    }
+    else {
+        printf("ORDINARY");
+    }
+    return 0;
+}
