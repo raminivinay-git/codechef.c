5669a72428f271a4d7837809954dccd3427c9066
VINAY <raminivinay777@gmail.com>
2026-05-26 17:54:36 +0530

    Thala For A Reason.c

---
+++ Thala For A Reason.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", & n);
+    if (n == 7) {
+        printf("THALA");
+    }
+    else {
+        printf("SADGE");
+    }
+    return 0;
+}
