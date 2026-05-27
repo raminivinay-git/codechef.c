c3b382efde7101bd0877e721bbafa23b19dc77bf
VINAY <raminivinay777@gmail.com>
2026-05-27 14:26:42 +0530

    Advitiya.c

---
+++ Advitiya.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", & n);
+    if (n == 16 || n == 17 || n == 18) {
+        printf("ADVITIYA");
+    }
+    else {
+        printf("WAITING FOR ADVITIYA");
+    }
+    return 0;
+}
