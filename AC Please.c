055e01dd1a5c611e3cb020123b54a45b7a79bfb5
VINAY <raminivinay777@gmail.com>
2026-05-27 14:30:25 +0530

    AC Please.c

---
+++ AC Please.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int temp;
+    scanf("%d", & temp);
+    if (temp > 30) {
+        printf("YES");
+    }
+    else {
+        printf("NO");
+    }
+    return 0;
+}
