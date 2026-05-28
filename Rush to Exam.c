6767a72c62cc41d24003f5b8069434717dc28663
VINAY <raminivinay777@gmail.com>
2026-05-28 10:29:37 +0530

    Rush to Exam.c

---
+++ Rush to Exam.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, a;
+    scanf("%d %d %d", & n, & m, & a);
+    if ((n * a) >= m) {
+        printf("YES");
+    }
+    else {
+        printf("NO");
+    }
+    return 0;
+}
