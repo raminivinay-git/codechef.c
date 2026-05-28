ba131d5870bb134e32f29ec3bdc01b73bf84f159
VINAY <raminivinay777@gmail.com>
2026-05-28 13:38:02 +0530

    Fitness.c

---
+++ Fitness.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+
+    while (t--) {
+        int x;
+        scanf("%d", & x);
+        printf("%d\n", x * 10);
+
+    }
+    return 0;
+}
