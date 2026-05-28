7bd448bf33f4b353f1fd0a575982e1bad36258e0
VINAY <raminivinay777@gmail.com>
2026-05-28 10:16:38 +0530

    Saving Taxes.c

---
+++ Saving Taxes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, t;
+    scanf("%d", & t);
+    while (t--) {
+        scanf("%d %d", & x, & y);
+
+        printf("%d\n", x - y);
+    }
+    return 0;
+}
