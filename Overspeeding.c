08d2e4c8b7d40a10e106fb516a143300d4536bbd
VINAY <raminivinay777@gmail.com>
2026-05-31 11:19:59 +0530

    Overspeeding.c

---
+++ Overspeeding.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int s;
+    scanf("%d", & s);
+    if (s > 40) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
+    return 0;
+}
