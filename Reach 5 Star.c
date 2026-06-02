b15e7a89fd0f1946b2c3607ebb11434997e7bf93
VINAY <raminivinay777@gmail.com>
2026-06-02 14:12:45 +0530

    Reach 5 Star.c

---
+++ Reach 5 Star.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", & x, & y);
+    if ((x + y) >= 2000) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
+    return 0;
+}
