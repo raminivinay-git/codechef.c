c6b390553431978673ce1eff0fef3f390919f635
VINAY <raminivinay777@gmail.com>
2026-06-02 14:08:29 +0530

    Capital Gain Tax.c

---
+++ Capital Gain Tax.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", & x, & y);
+    if (x == y) {
+        printf("SAME\n");
+    }
+    else if (x > y) {
+        printf("DECREASED\n");
+    }
+    else {
+        printf("INCREASED\n");
+    }
+    return 0;
+}
