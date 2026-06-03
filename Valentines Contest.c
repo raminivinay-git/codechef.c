5a8cfefd9bf479fc62c8b400405fbcdd4ede8a24
VINAY <raminivinay777@gmail.com>
2026-06-03 09:50:15 +0530

    Valentines Contest.c

---
+++ Valentines Contest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", & n);
+    if (n == 121) {
+        printf("likely\n");
+    }
+    else {
+        printf("unlikely\n");
+    }
+    return 0;
+}
