edcab646151b5122e772fb5c46945ed8560ecc14
VINAY <raminivinay777@gmail.com>
2026-06-03 11:31:48 +0530

    Leg Space.c

---
+++ Leg Space.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", & n, & m);
+    if (m == n) {
+        printf("NO\n");
+    }
+    else {
+        printf("YES\n");
+    }
+    return 0;
+}
