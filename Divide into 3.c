209cc4783507542626c570515fe554bbd26d6419
VINAY <raminivinay777@gmail.com>
2026-06-06 10:29:04 +0530

    Divide into 3.c

---
+++ Divide into 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", & n);
+    if (n % 3 == 0) {
+        printf("0\n");
+    }
+    else {
+        printf("1\n");
+    }
+    return 0;
+}
