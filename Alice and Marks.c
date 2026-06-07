08a3b28baca0782608761fbd3e06d7ffebf41939
VINAY <raminivinay777@gmail.com>
2026-06-07 11:34:43 +0530

    Alice and Marks.c

---
+++ Alice and Marks.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", & x, & y);
+    if (x >= (2 * y)) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
+    return 0;
+}
