fefd05c3831eb87eeb3c21e25594c7ed3435b396
VINAY <raminivinay777@gmail.com>
2026-06-07 11:46:59 +0530

    Time Complexity.c

---
+++ Time Complexity.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, t;
+    scanf("%d", & t);
+    while (t--) {
+        scanf("%d %d", & a, & b);
+        if (a > b) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
