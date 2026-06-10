b16cee4941bc98a7f6d449b753c358d37473d67b
VINAY <raminivinay777@gmail.com>
2026-06-10 10:28:37 +0530

    Expiring Bread.c

---
+++ Expiring Bread.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+    while (t--) {
+        int n, m, k;
+        scanf("%d %d %d", & n, & m, & k);
+        if ((m * k) >= n) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
