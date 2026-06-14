a23830ff0b137d4ba86d8786b242c43788f6fc05
VINAY <raminivinay777@gmail.com>
2026-06-14 12:05:10 +0530

    Janmansh and Coins.c

---
+++ Janmansh and Coins.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+    while (t--) {
+        int x, y;
+        scanf("%d %d", & x, & y);
+        printf("%d\n", (x * 10) + (y * 5));
+    }
+    return 0;
+}
