4137d73db4b29158d66f8e0e4d87ca084c0e0645
VINAY <raminivinay777@gmail.com>
2026-06-22 17:14:05 +0530

    Expert Setter.c

---
+++ Expert Setter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    float x, y;
+    scanf("%d", & t);
+    while (t--)
+    {
+        scanf("%f %f", & x, & y);
+        if ((y / x) * 100 >= 50)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
