95e0f62dc47f13c2157d31edc0d2589deea9bf10
VINAY <raminivinay777@gmail.com>
2026-06-23 19:18:21 +0530

    Puzzle Hunt.c

---
+++ Puzzle Hunt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    scanf("%d", & N);
+    if (N >= 6 && N <= 8)
+    {
+        printf("\nYes");
+    }
+    else
+    {
+        printf("\nNo");
+    }
+    return 0;
+}
