c1e1fbbdb3881f04469e1f5dbf5e77e28b439323
VINAY <raminivinay777@gmail.com>
2026-06-29 18:16:09 +0530

    Implement pizza cutting logic in Pizza Cutting.c

---
+++ Pizza Cutting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+    while (t--) {
+        int n;
+        scanf("%d", & n);
+        if (n % 2 == 0 || n == 1) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
