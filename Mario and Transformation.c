9ee8e62808c0e59eb1622fce34a3622d2123ea68
VINAY <raminivinay777@gmail.com>
2026-07-18 11:41:09 +0530

    Create Mario and Transformation.c

---
+++ Mario and Transformation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+    while (t--) {
+        int x;
+        scanf("%d", & x);
+        if (x % 3 == 0) {
+            printf("NORMAL\n");
+        }
+        else if (x % 3 == 1) {
+            printf("HUGE\n");
+        }
+        else if (x % 3 == 2) {
+            printf("SMALL\n");
+        }
+    }
+    return 0;
+}
