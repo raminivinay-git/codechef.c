1736062dc9fc21fc5d45a3f8348ae875c0ca9c15
VINAY <raminivinay777@gmail.com>
2026-07-24 18:32:04 +0530

    Add Move Grid.c for coordinate calculation

---
+++ Move Grid.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", & a, & b, & c, & d);
+    int A = a - c;
+    int B = b - d;
+    printf("%d %d", A,B);
+    return 0;
+}
