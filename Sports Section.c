d30598fd0ce8e55f6f9187bdf4dc1010cee0724b
VINAY <raminivinay777@gmail.com>
2026-07-25 18:14:59 +0530

    Add Sports Section program to check input value

---
+++ Sports Section.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d", & x);
+    if (x >= 8) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
+    return 0;
+}
