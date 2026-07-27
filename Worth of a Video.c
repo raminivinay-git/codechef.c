03cff4e001036fd2baeb3b1c9d5bc864ebf1bbd1
VINAY <raminivinay777@gmail.com>
2026-07-27 05:49:03 +0530

    Add program to calculate video worth based on seconds

---
+++ Worth of a Video.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+    while (t--) {
+        int s;
+        scanf("%d", & s);
+        printf("%d\n", 24000 * s);
+    }
+    return 0;
+}
